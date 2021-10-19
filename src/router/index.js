8e899900f5116f02fa5e2dea008b8d90c0d0b70e
atomalak <taha.tomalak@gmail.com>
2021-10-19 23:37:29 +0300

    Müşteri istekleri

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
--- src/views/DefinationPolicies.vue
+++ src/views/DefinationPolicies.vue
@@ -2,16 +2,13 @@
   <v-container>
     <h1 class="text-center mb-4">Poliçe Tanımı</h1>
     <AddNewPolicy @savePolicy="savePolicy($event)" />
-
     <v-row v-if="policies.length > 0">
       <v-col md="4" sm="3" lg="4" v-for="policy in policies" :key="policy">
         <v-card class="mx-auto" color="#26c6da" dark max-width="400">
           <v-card-title> {{ policy.name }} </v-card-title>
-          
         </v-card>
       </v-col>
     </v-row>
-  
   </v-container>
 </template>
 <script>
